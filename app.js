5d78b180d4034b7a80fac6eb48c9693022cc8822
MahnoorHuzaifa <tahirmahnoor82@gmail.com>
2023-04-12 01:41:26 +0500

    Assignment no 2 of JS

---
+++ app.js
@@ -0,0 +1,43 @@
+// // Question1
+
+let  num1 = 3;
+function num() {
+    let num2 = 8;
+    console.log(num2);
+    return function () {
+        console.log(num2)
+    }
+}
+
+//Question2
+
+let colours = ["Red", "Blue", "Green", "Yellow", "Brown", "Black"];
+function colours ()
+if (colours == "Black"){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+console.log(colours);
+
+//question 3
+
+let a = document.getElementById("para");
+a.innerText = "This is answer of question no 3";
+
+
+//question 4
+
+let b = document.getElementById("para1");
+b.innerHTML = "<ul> <li>Maths</li>  <li>Physics</li> <li>Computer</li></ul>"
+
+
+//question 5
+
+let c = document.getElementById("para2");
+c.innerHTML = "Answer of question no 5";
+c.style.backgroundColor = "Aqua";
+
+
+
